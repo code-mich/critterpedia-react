a50a80a54fc96f380fe949bf6e494b4743af331c
code-mich <codemwong@gmail.com>
2024-10-24 22:39:20 -0700

    Edit favicon and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import leafIcon from "../assets/Nook_Inc.png";
 
 export function Header() {
+	const handleScroll = (e) => {
+		window.scrollTo(0, 0);
+	};
 	return (
 		<header>
 			<nav>
-				<Link to="/">
-					<h1>Critterpedia</h1>
+				<Link to="/" onClick={handleScroll}>
+					<h1>
+						Critter<span>pedia</span>{" "}
+						<span className="leafIcon">
+							<img src={leafIcon} alt="Critterpedia icon" />
+						</span>
+					</h1>
 				</Link>
 			</nav>
 		</header>
